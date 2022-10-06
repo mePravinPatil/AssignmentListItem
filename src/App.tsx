5b3a235ea492b572325e241d2c2ee8c0ec09e699
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-10-06 23:23:37 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Image from './Components/Image';
+import Icon from './Components/Icon';
+import Label from './Components/Label';
+
+function App() {
+  return (
+    <div>
+      <Image />
+      <Label />
+      <Icon />
+    </div>
+  );
+}
+
+export default App;
