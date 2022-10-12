cfa22f381beb5a7862e83df86f1712e8c16e0a7e
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-10-12 18:28:38 +0530

    Update Label.tsx

--- src/Components/Label.tsx
+++ src/Components/Label.tsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
-import {Wrapper, StyledLabelH3, StyledLabelP} from "./StyledComponents/StyledLabel";
+import { Wrapper, StyledLabelH3, StyledLabelP } from "./StyledComponents/StyledLabel";
 
 type LabelProps = {
-    title : string,
-    subtitle : string,
+    title: string,
+    subtitle: string,
 }
 
-function Label(props : LabelProps) {
-    
+function Label(props: LabelProps) {
+
     const [title, setTitle] = useState(props.title);
     const [subtitle, setSubTitle] = useState(props.subtitle);
 
     return (
         <Wrapper>
-            <StyledLabelH3 onClick={() => setTitle('NewTitle')}> {title} </StyledLabelH3>
-            <StyledLabelP onClick={() => setSubTitle('NewSubTitle')}> {subtitle}</StyledLabelP>
+            <StyledLabelH3
+                onClick={() => setTitle('NewTitle')}
+                id ='clickTitle'> {title}
+            </StyledLabelH3>
+            <StyledLabelP
+                onClick={() => setSubTitle('NewSubTitle')}
+                id='clickSubTitle'> {subtitle}
+            </StyledLabelP>
         </Wrapper>
     )
 }
