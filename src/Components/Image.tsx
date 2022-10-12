e12ab35e1cfb73813a55ff7109c10f2655dbaed5
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-10-12 12:20:20 +0530

    Update Image.tsx

--- src/Components/Image.tsx
+++ src/Components/Image.tsx
@@ -1,9 +1,14 @@
 import StyledImage from './StyledComponents/StyledImage';
 
-function Image () {
+type ImageProps = {
+   src : string,
+   alt : string
+}
+
+function Image (props : ImageProps) {
+   
  return (
-    <StyledImage src='https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">
-    ' />
+    <StyledImage src={props.src} alt={props.alt}/>
  )
 }
 
