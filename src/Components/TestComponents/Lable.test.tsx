252c4f6606ee66b62a4c93cfa8a127b192b66484
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-10-12 18:27:32 +0530

    Update Lable.test.tsx

--- src/Components/TestComponents/Lable.test.tsx
+++ src/Components/TestComponents/Lable.test.tsx
@@ -2,15 +2,21 @@ import { render, screen } from '@testing-library/react';
 import Label from '../Label';
 
 describe('The Lable component', () => {
-  test('Title contains correct value', () => {
-    render(<Label />)
-    const testTitle = screen.getByText('List Item Title');
-    expect(testTitle).toBeInTheDocument();
-  })
+    test('Title contains correct value', () => {
+        render(<Label title='List Item Title' subtitle='List Item Title subtitle' />)
+        const testTitle = screen.getByText('List Item Title');
+        expect(testTitle).toBeInTheDocument();
+    })
 
-  test('Subtitle contains correct value', () => {
-    render(<Label />)
-    const testSubTitle = screen.getByText('List Item Title subtitle');
-    expect(testSubTitle).toBeInTheDocument();
-  })
-});
+    test('Subtitle contains correct value', () => {
+        render(<Label title='List Item Title' subtitle='List Item Title subtitle' />)
+        const testSubTitle = screen.getByText('List Item Title subtitle');
+        expect(testSubTitle).toBeInTheDocument();
+    })
+
+    // test('title event click', () => {
+    //     render(<Label title='List Item Title' subtitle='List Item Title subtitle' />)
+    //     const click = screen.getByTestId("clickTitle");
+    //     expect(click).toBeInTheDocument();
+    //     });
+    });
