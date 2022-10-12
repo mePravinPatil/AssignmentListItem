c8627e90ab90738c88faa412beafd07c7878e896
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-10-12 18:26:41 +0530

    Update Image.test.tsx

--- src/Components/TestComponents/Image.test.tsx
+++ src/Components/TestComponents/Image.test.tsx
@@ -1,16 +1,16 @@
-import { render } from '@testing-library/react';
+import { render,screen } from '@testing-library/react';
 import Image from '../Image';
 
 describe('The Image component', () => {
   test('alt contains correct value', () => {
-    render(<Image />)
-    const testImage = document.querySelector("img") as HTMLImageElement;
-    expect(testImage.alt).toContain("Image");
+    render(<Image src= 'https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">' alt='First Image'/>)
+    const testImage = screen.getByRole("img");;
+    expect(testImage).toHaveAttribute('alt','First Image');
   })
 
   test('src contains correct value', () => {
-    render(<Image />)
-    const testImage = document.querySelector("img") as HTMLImageElement;
-    expect(testImage.src).toContain('https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">');
+    render(<Image src= 'https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">' alt='First Image'/>)
+    const testImage = screen.getByRole("img");
+    expect(testImage).toHaveAttribute('src','https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">');
   })
 });
