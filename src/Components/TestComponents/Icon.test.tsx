b02a030e0456d197995bd8b78d568de60d435797
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-10-12 18:27:01 +0530

    Update Icon.test.tsx

--- src/Components/TestComponents/Icon.test.tsx
+++ src/Components/TestComponents/Icon.test.tsx
@@ -1,8 +1,8 @@
-import {render, screen} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import Icon from '../Icon';
 
 test('Icon contains correct value', () => {
     render(<Icon />)
-    const testIcon = screen.getByTestId('icon');
-    expect(testIcon).toBeVisible();
+    const testIcon = document.getElementById('icon');
+    expect(testIcon).toBeInTheDocument();
   });
