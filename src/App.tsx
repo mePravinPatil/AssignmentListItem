176a04352e0bc9d00678377e3f347bedbcd9942d
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-10-12 12:19:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import Image from './Components/Image';
 import Icon from './Components/Icon';
 import Label from './Components/Label';
 
+
 function App() {
+
+  const information : any = [
+    {
+      id: '1',
+      title: 'List Item Title',
+      subtitle : "List Item Title subtitle",
+      src: 'https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">',
+      alt: 'First Image'
+    }];
+
   return (
     <>
-      <Image />
-      <Label />
+      <Image src = {information[0].src} alt = {information[0].alt}/>
+      <Label title = {information[0].title} subtitle = {information[0].subtitle} />
       <Icon />
     </>
   );
