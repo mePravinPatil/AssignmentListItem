7eaf9176c503c7e59757e4cab77edab929c0872a
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-01 18:56:39 +0530

    Update Label.tsx

--- src/Components/Label.tsx
+++ src/Components/Label.tsx
@@ -1,28 +1,25 @@
-import { useState } from "react";
-import { Wrapper, StyledLabelH3, StyledLabelP } from "./StyledComponents/StyledLabel";
+import {
+  Wrapper,
+  StyledLabelH3,
+  StyledLabelP,
+} from "./StyledComponents/StyledLabel";
+import {ComponentStory, ComponentMeta } from '@storybook/react';
 
 type LabelProps = {
-    title: string,
-    subtitle: string,
-}
+  title: string;
+  /**
+   * we are using as subtitle
+   */
+  subtitle: string;
+};
 
-function Label(props: LabelProps) {
-
-    const [title, setTitle] = useState(props.title);
-    const [subtitle, setSubTitle] = useState(props.subtitle);
-
-    return (
-        <Wrapper>
-            <StyledLabelH3
-                onClick={() => setTitle('NewTitle')}
-                data-testid ='clickTitle'> {title}
-            </StyledLabelH3>
-            <StyledLabelP
-                onClick={() => setSubTitle('NewSubTitle')}
-                data-testid='clickSubTitle'> {subtitle}
-            </StyledLabelP>
-        </Wrapper>
-    )
+const Template: ComponentStory(typeof Label) = ({ title, subtitle }: LabelProps) {
+  return (
+    <Wrapper>
+      <StyledLabelH3 > {title}</StyledLabelH3>
+      <StyledLabelP > {subtitle}</StyledLabelP>
+    </Wrapper>
+  );
 }
 
 export default Label;
