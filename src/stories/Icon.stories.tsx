39de1358024c79d78985662029c70c973d0473c3
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-02 17:59:30 +0530

    Create Icon.stories.tsx

---
+++ src/stories/Icon.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, Story } from '@storybook/react';
+import Icon from '../../Components/Icon';
+
+export default {
+    title : "Icon",
+    component: Icon
+} as Meta<typeof Icon>
+
+
+export const Default = () => <Icon />
