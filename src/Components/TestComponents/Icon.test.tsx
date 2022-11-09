1a83ad65ab5a4e44d830daa11ed9aac21fbbc1fc
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-09 18:21:53 +0530

    Delete src/Components/TestComponents directory

--- src/Components/TestComponents/Icon.test.tsx
+++
@@ -1,8 +0,0 @@
-import {render} from '@testing-library/react';
-import Icon from '../Icon';
-
-test('Icon contains correct value', () => {
-    render(<Icon />)
-    const testIcon = document.getElementById('icon');
-    expect(testIcon).toBeInTheDocument();
-  });
