3b89801e24d4b95b5a3188b85a785448488ce3ef
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-09 18:27:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import './App.css';
-import Image from './Components/Image';
-import Icon from './Components/Icon';
-import Label from './Components/Label';
+import "./App.css";
+import Image from "./Components/Image/Image";
+import Icon from "./Components/Icon/Icon";
+import Label from "./Components/Label/Label";
 
-
-function App() {
-
-  const information : any = [
+const App = () => {
+  const information: any = [
     {
-      id: '1',
-      title: 'List Item Title',
-      subtitle : "List Item Title subtitle",
+      id: "1",
+      title: "List Item Title",
+      subtitle: "List Item Title subtitle",
       src: 'https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">',
-      alt: 'First Image'
-    }];
-
+      alt: "First Image"
+    },
+  ];
   return (
     <>
-      <Image src = {information[0].src} alt = {information[0].alt}/>
-      <Label title = {information[0].title} subtitle = {information[0].subtitle} />
+      <Image src={information[0].src} alt={information[0].alt} />
+      <Label title={information[0].title} subtitle={information[0].subtitle} />
       <Icon />
     </>
   );
