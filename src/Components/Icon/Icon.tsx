7a481d98908b4569969ec6e91c56fb1f2a30c48a
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-09 18:23:05 +0530

    Create Icon.tsx

---
+++ src/Components/Icon/Icon.tsx
@@ -0,0 +1,12 @@
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledIcon from './StyledIcon';
+
+const Icon = () => {
+    return (
+        <StyledIcon id='icon'>
+            <MailOutlineIcon />
+        </StyledIcon>
+     )
+}
+
+export default Icon;
