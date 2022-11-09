82bd96f14af9ca304b34855add1afb3bb7d4613f
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-09 18:23:38 +0530

    Add files via upload

---
+++ src/Components/Icon/Icon.test.tsx
@@ -0,0 +1,8 @@
+import {render} from '@testing-library/react';
+import Icon from './Icon';
+
+test('Icon contains correct value', () => {
+    render(<Icon />)
+    const testIcon = document.getElementById('icon');
+    expect(testIcon).toBeInTheDocument();
+  });
---
+++ src/Components/Icon/StyledIcon.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledIcon = styled.div`
+margin : 20px 20px 0 0;
+border : none;
+height : auto;
+width : auto;
+`
+
+export default StyledIcon;
