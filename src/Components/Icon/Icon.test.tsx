6379dfe546d244bb693cdab42536e49e3335fa87
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-15 17:51:36 +0530

    Update Icon.test.tsx

--- src/Components/Icon/Icon.test.tsx
+++ src/Components/Icon/Icon.test.tsx
@@ -2,7 +2,7 @@ import {render} from '@testing-library/react';
 import Icon from './Icon';
 
 test('Icon contains correct value', () => {
-    render(<Icon />)
+    render(<Icon iconname=''/>)
     const testIcon = document.getElementById('icon');
     expect(testIcon).toBeInTheDocument();
   });
