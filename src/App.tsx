befef9693a6c5736e79dd9bd58c8156b55170248
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-15 17:50:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import "./App.css";
+import './App.css';
 import { Image, Icon, Label } from "./Components";
 
 const App = () => {
-  const information: any = [
+  const information: {
+    id: string;
+    title: string;
+    subtitle: string;
+    src: string;
+    alt: string;
+    iconname: string;
+  }[] = [
     {
       id: "1",
       title: "List Item Title",
       subtitle: "List Item Title subtitle",
       src: 'https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">',
-      alt: "First Image"
+      alt: "First Image",
+      iconname: 'fa-regular fa-envelope'
     },
   ];
   return (
     <>
       <Image src={information[0].src} alt={information[0].alt} />
-      <Label title={information[0].title} subtitle={information[0].subtitle} />
-      <Icon />
+      <div className="label">
+        <Label text={information[0].title} isTitle={true} />
+        <Label text={information[0].subtitle} isTitle={false} />
+      </div>
+      <Icon iconname={information[0].iconname} />
     </>
   );
-}
+};
 
 export default App;
