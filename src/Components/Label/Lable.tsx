4d6aa7864a0efaf009aff78d9edd04603f6abca6
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-15 17:54:27 +0530

    Update Lable.tsx

--- src/Components/Label/Lable.tsx
+++ src/Components/Label/Lable.tsx
@@ -1,24 +1,21 @@
-import {
-  StyledLabel,
-} from "./Label.style";
+import { StyledLabel } from "./Label.style";
 
- export type LabelProps = {
+export type LabelProps = {
   /**
    * we are using as title
    */
-  text: string;
+  text?: string;
   /**
    * we are using as subtitle
    */
   isTitle: boolean;
-
 };
 
 const Label = ({ text, isTitle }: LabelProps) => {
+  let newtext = "Title is not Available";
   return (
-      <StyledLabel isTitle={isTitle}> {text}</StyledLabel>
- 
+    <StyledLabel isTitle={isTitle}> {text !== "" ? text : newtext}</StyledLabel>
   );
-}
+};
 
 export default Label;
