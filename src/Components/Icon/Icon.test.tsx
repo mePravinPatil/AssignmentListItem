137ebc5c811c6998d13e13bd7a3982c767fc3eca
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-16 14:19:54 +0530

    Update Icon.test.tsx

--- src/Components/Icon/Icon.test.tsx
+++ src/Components/Icon/Icon.test.tsx
@@ -2,7 +2,16 @@ import {render} from '@testing-library/react';
 import Icon from './Icon';
 
 test('Icon contains correct value', () => {
-    render(<Icon iconname=''/>)
-    const testIcon = document.getElementById('icon');
-    expect(testIcon).toBeInTheDocument();
+  render(<Icon iconname=''/>)
+  const testIcon = document.getElementById('icon');
+  expect(testIcon).toBeInTheDocument();
+});
+
+
+test('Icon contains correct value in Props', () => {
+    render(<Icon />)
+    const testIcon = document.getElementById('test');
+    expect(testIcon).toHaveClass('fa-solid fa-envelope');
   });
+
+  
