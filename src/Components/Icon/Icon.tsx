e297e936b897cbdd700c76c3fd3710b03276b947
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-16 14:20:20 +0530

    Update Icon.tsx

--- src/Components/Icon/Icon.tsx
+++ src/Components/Icon/Icon.tsx
@@ -3,16 +3,16 @@ import StyledIcon from "./Icon.style";
 
 type IconProp = {
     /**
-   * we are using it for Icon classname
+   * we are using it for Icon class-name
    */
   iconname?: string;
 };
 
-const Icon = ({iconname}: IconProp) => {
+const Icon = ({iconname = 'fa-solid fa-envelope'}: IconProp) => {
   
   return (
     <StyledIcon id="icon">
-      <i className={iconname !== ''? iconname : 'fa-solid fa-envelope'}></i>
+      <i id='test' className={iconname }></i>
     </StyledIcon>
   );
 };
