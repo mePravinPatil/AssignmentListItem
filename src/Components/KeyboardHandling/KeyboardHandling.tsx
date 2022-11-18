2591adce87e41bfc682bac518d42b87b1475dcc5
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-18 18:11:18 +0530

    Update KeyboardHandling.tsx

--- src/Components/KeyboardHandling/KeyboardHandling.tsx
+++ src/Components/KeyboardHandling/KeyboardHandling.tsx
@@ -1,12 +1,37 @@
-const list = ['FirstData', 'SecondData', 'ThirdData', 'ForthData'];
+import React, { useState } from "react";
 
-const List = () => {
+const KeyboardHandling = () => {
+
+  const [enteredText, setEnteredText] = useState("");
+
+  
+  const keyDownHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.code === "Enter") {
+      alert(`You have entered "${enteredText}"`);
+    }
+  };
+
+  const keyUpHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.code === "Escape") {
+      const confirm = window.confirm("Are you sure want to clear this text?");
+
+      if (confirm) {
+        setEnteredText("");
+      }
+    }
+  };
+
   return (
     <div>
-      {list.map((item) => (
-        <div key={item}>{item}</div>
-      ))}
+      <input
+        onKeyDown={keyDownHandler}
+        onKeyUp={keyUpHandler}
+        type="text"
+        value={enteredText}
+        onChange={(e) => setEnteredText(e.target.value)}
+      />
     </div>
   );
-      };
+};
 
+export default KeyboardHandling;
