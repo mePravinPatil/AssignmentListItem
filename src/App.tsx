1b087a0364ace1f6cf4f538e97c5b98203b49ac2
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-21 18:29:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import './App.css';
-import { Image, Icon, Label } from "./Components";
+import "./App.css";
+import { Image, Icon, Label, KeyboardHandling } from "./Components";
 
 const App = () => {
+  const listArrayItems :string[] = [
+    "First",
+    "second",
+    "Third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+  ];
+
   const information: {
     id: string;
     title: string;
@@ -16,17 +26,18 @@ const App = () => {
       subtitle: "List Item Title subtitle",
       src: 'https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBvZmZpY2V8ZW58MHx8MHx8&w=1000&q=80">',
       alt: "First Image",
-      iconname: 'fa-regular fa-envelope'
+      iconname: "fa-regular fa-envelope",
     },
   ];
   return (
     <>
-      <Image src={information[0].src} alt={information[0].alt} />
+      {/* <Image src={information[0].src} alt={information[0].alt} />
       <div className="label">
         <Label text={information[0].title} isTitle={true} />
         <Label text={information[0].subtitle} isTitle={false} />
       </div>
-      <Icon iconname={information[0].iconname} />
+      <Icon iconname={information[0].iconname} /> */}
+      <KeyboardHandling listArrayItems = {listArrayItems}/>
     </>
   );
 };
