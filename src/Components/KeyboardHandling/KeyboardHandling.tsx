4e7baf0b6b0f6c0524a574a490f8dfb062bb503b
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2022-11-24 19:33:42 +0530

    Update KeyboardHandling.tsx

--- src/Components/KeyboardHandling/KeyboardHandling.tsx
+++ src/Components/KeyboardHandling/KeyboardHandling.tsx
@@ -1,49 +1,107 @@
-import { useState } from "react";
-import "./KeyboardHandling.css";
+import React, { useEffect, useState, useReducer } from "react";
+import "./KeyboardHandling.style.ts";
+import List from "./List";
 
-type ArrayProps = {
-  listArrayItems: {name : string}[];
+const list:string[] = ["Item One", "Item Two", "Item Three", "Item Four", "Item Five"];
+
+const initialState = { selectedIndex: 0 };
+
+const useKeyPress = (targetKey: string) => {
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  type PropType = {
+    key: string;
+  };
+
+  useEffect(() => {
+    const downHandler = ({ key }: PropType) => {
+      if (key === targetKey) {
+        setKeyPressed(true);
+      }
+    };
+
+    const upHandler = ({ key }: PropType) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    };
+
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, [targetKey]);
+
+  return keyPressed;
 };
 
-const KeyboardHandling = ({listArrayItems} : ArrayProps) => {
-
-let myList = document.getElementById('myList');
-
-myList.onkeydown = function(event) {
-
-  var numberOfCells = document.getElementById('myList').getElementsByTagName("ul").length;
-  if (event.keyCode == 38) {
-    console.log('up');
-    document.getElementById(event.target.id).blur()
-    var currentfocus = event.target.id.split('');
-    currentfocus.splice(2, 1, +currentfocus[2]-1);
-    var newfocus = currentfocus.join('');
-    document.getElementById(newfocus).focus();
-  }
-  else if (event.keyCode == 40) {
-    console.log('down');
-    document.getElementById(event.target.id).blur()
-    var currentfocus = event.target.id.split('');
-    currentfocus.splice(2,1, +currentfocus[2]+1);
-    var newfocus = currentfocus.join('');
-    document.getElementById(newfocus).focus();
+const reducer = (state:{selectedIndex:number}, action: any) => {
+  
+  switch (action.type) {
+    case "arrowUp":
+      return {
+        selectedIndex:
+          state.selectedIndex !== 0 ? state.selectedIndex - 1 : list.length - 1,
+      };
+    case "arrowDown":
+      return {
+        selectedIndex:
+          state.selectedIndex !== list.length - 1 ? state.selectedIndex + 1 : 0,
+      };
+      case "End":
+      return {
+        selectedIndex:
+          state.selectedIndex = list.length - 1,
+      };
+      case "Home":
+      return {
+        selectedIndex:
+          state.selectedIndex = 0,
+      };
+    case "select":
+      return { selectedIndex: action.payload };
+    default:
+      throw new Error();
   }
 };
+
+ 
+const KeyboardHandling = () => {
+  const arrowUpPressed = useKeyPress("ArrowUp");
+  const arrowDownPressed = useKeyPress("ArrowDown");
+  const endPressed = useKeyPress("End");
+  const homePressed = useKeyPress("Home");
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    if (arrowUpPressed) {
+      dispatch({ type: "arrowUp" });
+    }
+  }, [arrowUpPressed]);
+
+  useEffect(() => {
+    if (arrowDownPressed) {
+      dispatch({ type: "arrowDown" });
+    }
+  }, [arrowDownPressed]);
+
+  useEffect(() => {
+    if (endPressed) {
+      dispatch({ type: "End" });
+    }
+  }, [endPressed]);
+
+  useEffect(() => {
+    if (homePressed) {
+      dispatch({ type: "Home" });
+    }
+  }, [homePressed]);
 
   return (
-    <div>
-      <ul id="myList">
-        {listArrayItems.map((e, key) => {
-          return (
-            <li key={key} className="parent-list">
-              <ul>
-                <li >{e.name}</li>
-              </ul>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <List list={list} state={state} dispatch={dispatch}/>
   );
 };
 
